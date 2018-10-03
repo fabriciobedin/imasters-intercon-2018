361714cbb0cc21ed4b802f2b95e4fb68e99a8a0d
ricardo.ogliari@megaleios.com <rogliariping@gmail.com>
2018-10-03 18:34:46 -0300

    adição de cloud functions para push com mudança de energia

--- FirebaseCloudFuncions/functions/index.js
+++ FirebaseCloudFuncions/functions/index.js
@@ -1,20 +1,63 @@
 const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
 
-exports.addMessage = functions.https.onRequest((req, res) => {
-  const original = req.query.text;
-  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-    return res.redirect(303, snapshot.ref.toString());
-  });
-});
+// See documentation on defining a message payload.
+var payload = {
+  data: {
+    value: 1
+  }
+};
 
+exports.alertChangeEnergy = 
+  functions.database.ref('/sensorEnergia/status/status')
+    .onUpdate((snapshot, context) => {
+      var newState = snapshot.val();
+      payload.data.value = newState;
 
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-    .onCreate((snapshot, context) => {
-      const original = snapshot.val();
-      console.log('Uppercasing', context.params.pushId, original);
-      const uppercase = original.toUpperCase();
-      return snapshot.ref.parent.child('uppercase').set(uppercase);
+      admin.database().ref('/smartphones').once('value').then(
+            function(snapshot) {
+              var tokens = snapshot.val();
+              if(tokens){
+                  var currentToken;
+                  var data = [];
+                  for(var key in tokens){
+                    currentToken = tokens[key]
+                    admin.messaging().sendToDevice(currentToken, payload)
+                    .then(success => {
+                        return;
+                    })
+                    .catch(error => {
+                        return;
+                    });
+                  }
+              }
+            });
+
+      return;
     });
+
+exports.stopAlarm = functions.https.onRequest((request, response) => {
+  admin.database().ref('/token').once('value').then(
+      function(snapshot) {
+        var registrationToken = snapshot.val();
+
+        admin.messaging().sendToDevice(registrationToken, payload)
+        .then(success => {
+            response.send("{'sucess': true}");
+            return;
+        })
+        .catch(error => {
+            response.send("{'sucess': false}");
+            return;
+        });
+
+        return;
+      }
+  ).catch(error => {
+            response.send("{'sucess': false}");
+            return;
+        });
+  
+});
